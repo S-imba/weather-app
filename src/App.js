f63dd027604cc1276aab892f071c4c7fbfe52cdd
Simba Wangulu <simbawangulu@gmail.com>
2022-02-22 16:07:05 +0300

    Create react app and added weather api key

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+
+const weather_API_Key = {
+  key : "908e1279e9312c592fb9f563f5ce02a5",
+  base : "https://api.openweathermap.org/data/2.5/"
+};
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
